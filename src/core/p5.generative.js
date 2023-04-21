9e0db06b528d6b1990cf853892d2fed319adbfdf
johnr0 <jochung@adobe.com>
2023-04-20 23:29:50 -0700

    frame interpolation & an example

--- src/core/p5.generative.js
+++ src/core/p5.generative.js
@@ -5,15 +5,15 @@
  */
 
 import p5 from './main';
-import * as constants from './constants';
 
 
 /**
  * TODO: add description
  *
  * @method arc
+ * @param  {String} endpoint        the url of the ML endpoint
  * @param  {p5.Graphics} g          the graphics object from which we will draw the source image
-
+ * @param  {Object} target          the dictionary about the target. should contain target object to draw the generated image to, x and y position about where to draw the image 
  * @param  {String} prompt          prompt to specify the generation
  * @param  {String} negative_prompt negative prompt to specify the generation
  * @param  {Number} strength        strength of the img2img
@@ -26,89 +26,58 @@ import * as constants from './constants';
  * @chainable
  *
  * @example
- * 
  * TODO: add descriptions
- * 
 **/
-p5.prototype.generate = async function(endpoint, g, target, prompt, negative_prompt='', prompt_weights=[], negative_prompt_weights=[], strength=0.7, cfg=7.5, sampler='DDIM', seed=undefined, steps=16) {
-    // console.log(g)
-    // console.log(g.canvas, g._renderer)
-    var ctx
-    var offscreenCanvas = document.createElement("canvas");
-    offscreenCanvas.width = g.canvas.width;
-    offscreenCanvas.height = g.canvas.height;
-    // var offscreenCtx = offscreenCanvas.getContext("2d");
-    // offscreenCtx.fillStyle='white'
-    // offscreenCtx.fillRect(0,0,g.canvas.width,g.canvas.height);
-    ctx = offscreenCanvas.getContext("2d");
-    ctx.drawImage(g.canvas,0,0);
-    
-    if (typeof prompt == 'string'){
-        prompt = [prompt]
-    }
-    if (typeof negative_prompt == 'string'){
-        negative_prompt = [negative_prompt]
-    }
-    const imgdata = offscreenCanvas.toDataURL("image/png");
-    console.log(imgdata)
-
-    var response = await fetch(endpoint+'/img2img', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-        'init_img': imgdata, 
-        'prompt': prompt,
-        'negative_prompt': negative_prompt,
-        'prompt_weights': prompt_weights,
-        'negative_prompt_weights': negative_prompt_weights,
-        'strength': strength,
-        'cfg': cfg,
-        'seed': seed,
-        'steps': steps})
-
+p5.prototype.generate = async function(endpoint, g, target, prompt, negative_prompt='', prompt_weights=[], negative_prompt_weights=[], strength=0.7, cfg=7.5, sampler='DDIM', seed=undefined, steps=16, export_frame=undefined) {
+  var offscreenCanvas = document.createElement('canvas');
+  offscreenCanvas.width = g.canvas.width;
+  offscreenCanvas.height = g.canvas.height;
+  var ctx = offscreenCanvas.getContext('2d');
+  ctx.drawImage(g.canvas,0,0);
+  if (typeof prompt == 'string'){
+    prompt = [prompt];
+  }
+  if (typeof negative_prompt == 'string'){
+    negative_prompt = [negative_prompt];
+  }
+  const imgdata = offscreenCanvas.toDataURL('image/png');
+  console.log(imgdata);
+  var response = await fetch(endpoint+'/img2img', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      'init_img': imgdata,
+      'prompt': prompt,
+      'negative_prompt': negative_prompt,
+      'prompt_weights': prompt_weights,
+      'negative_prompt_weights': negative_prompt_weights,
+      'strength': strength,
+      'cfg': cfg,
+      'seed': seed,
+      'steps': steps
     })
-    
-    var data = await response.json()
-
-    var img = new Image();
-    // img.onload = function(){
-    //     console.log(img.width, img.height)
+  });
+  var data = await response.json();
+  var img = new Image();
+  img.src = data['img'];
+  await img.decode();
+  g.clear()
+  g.canvas.getContext('2d').drawImage(img, 0, 0, img.width, img.height,0, 0, g.width, g.height);
+  if(target!==undefined){
+    try{
+        await target['target'].image(g, target['x'], target['y'], target['width'], target['height']);
         
-    // }
-    img.src = data['img']
-    await img.decode();
-    g.canvas.getContext('2d').drawImage(img, 0, 0, img.width, img.height,0, 0, g.width, g.height);
-    if(target!=undefined){
-        console.log(target['target'])
-        target['target'].image(g, target['x'], target['y'], g.width, g.height)
     }
-
-    // .then((response) => {
-    //     console.log(response)
-    //     return response.json()
-    // }).then((data) => {
-    //     // console.log(data['img'])
-    //     console.log('img received')
-    //     var img = new Image();
-    //     img.onload = function(){
-    //         console.log(img.width, img.height)
-    //         g.canvas.getContext('2d').drawImage(img, 0, 0, img.width, img.height,0, 0, g.width, g.height);
-    //         if(target!=undefined){
-    //             target['target'].image(g, target['x'], target['y'], g.width, g.height)
-    //         }
-    //         // p5.prototype.image(g, 0, 0);
-    //         // g.image(img, 0, 0)
-    //         // p5.Renderer2D.prototype.image(g, 0,0)
-    //     }
-    //     img.src = data['img']
-        
-    // })
-
-    // TODO send the image data... and retrieve the generated image data
-    
-    return g
-  };
-
-  export default p5;
+    catch{
+        console.log('target is not fully specified.');
+    }
+    if(export_frame!=undefined){
+        console.log('exporting?')
+        target['target'].saveCanvas(target['target'].canvas, str(export_frame), '.png');
+    }
+  }
+  return g;
+};
+export default p5;
---
+++ test/frames2vid.py
@@ -0,0 +1,21 @@
+from moviepy.editor import ImageSequenceClip
+import os
+
+# Set the directory where the image files are stored
+image_dir = "./test_results/04202023_2"
+
+# Load the image files into a list
+files = os.listdir(image_dir)
+files.sort(key=lambda x: os.path.getmtime(os.path.join(image_dir, x)))
+
+images = [f"{image_dir}/{img}" for img in files if img.endswith(".png")]
+# print(images)
+# Create a video clip from the images
+clip = ImageSequenceClip(images, fps=30)
+
+# Set the filename and format of the output video
+output_file = "/path/to/output.mp4"
+
+# Write the video file
+output_file = image_dir+"/output.mp4"
+clip.write_videofile(output_file, fps=30)
